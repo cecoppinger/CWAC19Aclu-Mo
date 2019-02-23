3cba72554ac92ea997f8e7518acc809e01cfa6ab
Clarence Coppinger <thea.trio@gmail.com>
2019-02-23 17:21:25 -0600

    Add new complaint view/controller action

--- CWAC19Aclu-Mo/Controllers/ComplaintsController.cs
+++ CWAC19Aclu-Mo/Controllers/ComplaintsController.cs
@@ -19,19 +19,42 @@ namespace CWAC19Aclu_Mo.Controllers
 
         public IActionResult New()
         {
-            return View();
+            AddComplaintViewModel viewModel = new AddComplaintViewModel(_context.ComplaintStatuses.ToList());
+            return View(viewModel);
         }
 
-        //[HttpPost]
-        //public IActionResult New(AddComplaintViewModel viewModel)
-        //{
-        //    if(ModelState.IsValid)
-        //    {
-        //        ClientProfile newClient = new ClientProfile(viewModel.Email);
+        [HttpPost]
+        public IActionResult New(AddComplaintViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ClientProfile client = ClientExists(viewModel.Email);
+                if(client == null)
+                {
+                    client = new ClientProfile(viewModel.Email);
+                    _context.ClientProfiles.Add(client);
+                }
 
+                Complaint newComplaint = new Complaint
+                {
+                    ClientProfileID = client.ID,
+                    ComplaintStatusID = viewModel.StatusCodeID,
+                    ComplaintNo = viewModel.ComplaintNo
+                };
 
-        //    }
-        //}
+                _context.Complaints.Add(newComplaint);
+                _context.SaveChanges();
+
+                return RedirectToAction("New");
+            }
+
+            return View(viewModel);
+        }
+
+        private ClientProfile ClientExists(string email)
+        {
+            return _context.ClientProfiles.SingleOrDefault(u => u.Email == email);
+        }
 
     }
 }
