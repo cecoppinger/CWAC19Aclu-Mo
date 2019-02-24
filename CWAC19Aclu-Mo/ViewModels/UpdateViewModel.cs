a7f8cd92977e328ac223472aabb507ab56690375
joshuashaffer189 <jshaffer189@gmail.com>
2019-02-23 19:13:48 -0600

    merge Update/josh

---
+++ CWAC19Aclu-Mo/ViewModels/UpdateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using CWAC19AcluMo.Models;
+
+namespace CWAC19AcluMo.ViewModels
+{
+    public class UpdateViewModel
+    {
+        public string Email { get; set; }
+
+        public IList<Complaint> complaints { get; set; }
+    } 
+}
